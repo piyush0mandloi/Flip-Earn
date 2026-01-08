fa13cc47dbba374908980ace625b7ead3e6389b2
Piyush Mandloi <piyushmandloi474@gmail.com>
2026-01-08 19:05:07 +0530

    chat day2

--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -1,14 +1,47 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState,useMemo } from 'react'
 import { dummyChats } from '../assets/assets';
 import { MessageCircle, Search } from 'lucide-react';
+import {format, isToday, isYesterday, parseISO} from 'date-fns'
+import {useDispatch} from 'react-redux'
+import { setChat } from "../app/features/chatSlice";
 
 const Messages = () => {
+
+  const dispatch = useDispatch()
 
   const user = {id:"user_1"}
 
   const [chats, setChats] = useState([])
   const [searchQuery, setSearchQuery] = useState('');
   const [loading, setLoading] = useState(true);
+
+  const formatTime=(dateString)=>{
+    if(!dateString) return;
+    
+    const date = parseISO(dateString)
+
+    if(isToday(date)){
+      return 'Today' + format(date, "HH:mm")
+    }
+    if(isYesterday(date)){
+      return 'Yesterday'+ format(date, "HH:mm")
+    }
+    return format(date, "MMM d")
+  }
+
+
+  const filteredChats=useMemo(()=>{
+    const query=searchQuery.toLowerCase();
+    return chats.filter((chat)=>{
+      const chatUser = chat.chatUserId === user?.id ? chat?.ownerUser : chat?.chatUser;
+
+      return chat.listing?.title?.toLowerCase().includes(query) || chatUser?.name?.toLowerCase().includes(query)
+    })
+  },[chats, searchQuery])
+
+  const handleOpenChat = (chat)=>{
+    dispatch(setChat({listing: chat.listing, chatId:chat.id}))
+  }
 
   const fetchUserChats =async ()=>{
     setChats(dummyChats)
@@ -41,14 +74,39 @@ const Messages = () => {
         {/* Chat List */}
         {loading ? (
           <div className='text-center text-gray-500 py-20'>Loading messages...</div>
-        ): chats.length===0 ? (
+        ): filteredChats.length===0 ? (
           <div className='bg-white rounded-lg shadow-xs border border-gray-200 p-16 text-center'>
             <div className='w-16 h-16 bg-gray-100 rounded-full flex items-center justify-center mx-auto mb-4'>
               <MessageCircle className='w-8 h-8 text-gray-400'/>
             </div>
+            <h3 className='text-xl font-medium text-gray-800 mb-2'>{searchQuery ? 'No chats found' : 'No messages yet'}</h3>
+            <p className='text-gray-600'>
+              {searchQuery ? 'Try a diffrent search term' : 'Start a conversation by viewing a listing and clicking "Chat with Seller"'}
+            </p>
           </div>
         ):(
-          <div></div>
+          <div className='bg-white rounded-lg shadow-xs border border-gray-200 divide-y divide-gray-200'>
+            { filteredChats.map((chat)=>{
+              const chatUser = chat.chatUserId === user ?.id ? chat.ownerUser : chat.chatUser;
+              return (
+                <button onClick={()=> handleOpenChat(chat)} key={chat.id} className='w-full p-4 hover:bg-gray-50 transition-colors text-left'>
+                  <div className='flex items-start space-x-4'>
+                    <div className='flex-shrink-0'>
+                      <img src={chatUser?.image} alt={chat?.chatUser?.name} className='w-12 h-12 rounded-lg object-cover'/>
+                    </div>
+                    <div className='flex-1 min-w-0'>
+                      <div className='flex items-center justify-between mb-1'>
+                        <h3 className='font-semibold text-gray-800 truncate'>{chat.listing?.title}</h3>
+                        <span className='text-xs text-gray-500 flex-shrink-0 ml-2'>{formatTime (chat.updatedAt)}</span>
+                      </div>
+                      <p className='text-sm text-gray-500 truncate mb-1'>{chatUser?.name}</p>
+                      <p className={`text-sm truncate ${!chat.isLastMessageRead && chat.lastMessageSenderId !== user?.id ? 'text-indigo-600 font-medium': 'text-gray-500'}`}>{chat.lastMessage || 'No messages yet'}</p>
+                    </div>
+                  </div>
+                </button>
+              )
+            })}
+          </div>
         )}
       </div>
     </div>
