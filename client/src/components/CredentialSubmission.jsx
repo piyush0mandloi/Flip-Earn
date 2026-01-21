4f24ea49eb1336ef752f7021e572527355c90816
Piyush Mandloi <piyushmandloi474@gmail.com>
2026-01-21 15:12:35 +0530

    creating amount components

--- client/src/components/CredentialSubmission.jsx
+++ client/src/components/CredentialSubmission.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import toast from 'react-hot-toast'
+import {CirclePlus,X} from 'lucide-react'
 
 const CredentialSubmission = ({onClose, listing}) => {
     const [newField, setNewField] = useState("")
@@ -21,13 +22,33 @@ const CredentialSubmission = ({onClose, listing}) => {
     <div className='fixed inset-0 bg-black/70 backdrop-blur bg-opacity-50 z-100 flex items-center justify-center sm:p-4'>
       <div className='bg-white sm:rounded-lg shadow-2xl w-full max-w-lg h-screen sm:h-[320px] flex flex-col'>
         {/* Header */}
-        <div>
-            <div>
+        <div className='bg-gradient-to-r from-indigo-600 to-indigo-400 text-white p-4 sm:rounded-t-lg flex items-center justify-between'>
+            <div className='flex-1 min-w-0'>
                 <h3>{listing?.title}</h3>
                 <p>Adding Credentials for {listing?.username} on {listing?.platform}</p>
             </div>
-            <button><X/></button>
+            <button onClick={onClose} className='ml-4 p-1 hover:bg-white/20 hover:bg-opacity-20 rounded-lg transition-colors'><X className="w-5 h-5"/></button>
         </div>
+
+        {/* Form */}
+        <form onSubmit={handleSubmission} className='flex flex-col items-start gap-4 p-4 overflow-y-scroll'>
+          {credential.map((cred, index)=>(
+            <div className='grid grid-cols-[2fr_3fr_1fr] items-center gap-2' key={cred.type}>
+              <label className='text-sm font-medium text-gray-800'>{cred.name}</label>
+              <input type="text" value={cred.value} onChange={(e)=> setCredential((prev)=>prev.map((c,i)=>(i===index)?{...c,value:e.target.value}: c))} className='w-full px-2 py-1.5 text-sm border border-gray-300 rounded outline-indigo-400'/>
+              <X className="w-5 h-5 text-gray-500 hover:text-gray-700 cursor-pointer" onClick={()=> setCredential((prev)=>prev.filter((_,i)=>i!==index))}/>
+            </div>
+          ))}
+          {/* Add more fields */}
+          <div className='flex items-center gap-2'>
+            <input type="text" value={newField} onChange={(e)=> setNewField(e.target.value)} placeholder='field name...' className='outline-none border-b border-gray-200' />
+            <button type='button' onClick={handleAddField} className='flex items-center gap-2 text-sm text-gray-600 hover:text-gray-700 cursor-pointer'>
+              <CirclePlus className='w-5 h-5'/>
+            </button>
+          </div>
+          {/* Submit */}
+          <button className='bg-indigo-600 hover:bg-indigo-700'>Submit</button>
+        </form>
       </div>
     </div>
   )
