e062e07adad13cc369ecd38633dc64eec2c6362e
Piyush Mandloi <piyushmandloi474@gmail.com>
2026-01-30 01:56:08 +0530

    Frontend i Completed . Now Moving to Backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,32 @@
-import React from 'react'
-import { Routes, Route, useLocation } from 'react-router-dom'
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 // Pages
-import Home from './pages/Home'
-import Marketplace from './pages/Marketplace'
-import MyListings from './pages/MyListings'
-import ListingDetails from './pages/ListingDetails'
-import ManageListing from './pages/ManageListing'
-import Messages from './pages/Messages'
-import MyOrders from './pages/MyOrders'
-import Loading from './pages/Loading'
-import Navbar from './components/Navbar'
-import ChatBox from './components/ChatBox'
-import {Toaster} from 'react-hot-toast'
+import Home from "./pages/Home";
+import Marketplace from "./pages/Marketplace";
+import MyListings from "./pages/MyListings";
+import ListingDetails from "./pages/ListingDetails";
+import ManageListing from "./pages/ManageListing";
+import Messages from "./pages/Messages";
+import MyOrders from "./pages/MyOrders";
+import Loading from "./pages/Loading";
+import Navbar from "./components/Navbar";
+import ChatBox from "./components/ChatBox";
+import { Toaster } from "react-hot-toast";
+import Layout from "./pages/admin/Layout";
+import Dashboard from "./pages/admin/Dashboard";
+import CredentialVerify from "./pages/admin/CredentialVerify";
+import CredentialChange from "./pages/admin/CredentialChange";
+import AllListings from "./pages/admin/AllListings";
+import Transactions from "./pages/admin/Transactions";
+import Withdrawal from "./pages/admin/Withdrawal";
 
 const App = () => {
-  const {pathname} = useLocation()
+  const { pathname } = useLocation();
   return (
     <div>
       <Toaster />
-      {!pathname.includes('/admin') && <Navbar/>}
+      {!pathname.includes("/admin") && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/marketplace" element={<Marketplace />} />
@@ -40,10 +47,19 @@ const App = () => {
 
         {/* Loading */}
         <Route path="/loading" element={<Loading />} />
-      </Routes>
-      <ChatBox/>
-    </div>
-  )
-}
 
-export default App
+        <Route path="/admin" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="verify-credentials" element={<CredentialVerify />} />
+          <Route path="change-credentials" element={<CredentialChange />} />
+          <Route path="list-listings" element={<AllListings />} />
+          <Route path="transactions" element={<Transactions />} />
+          <Route path="withdrawal" element={<Withdrawal />} />
+        </Route>
+      </Routes>
+      <ChatBox />
+    </div>
+  );
+};
+
+export default App;
