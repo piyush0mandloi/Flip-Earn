93fad1fc76e50969e301a88cbecb2e3444bdb895
Piyush Mandloi <piyushmandloi474@gmail.com>
2026-02-18 02:22:37 +0530

    something happend

---
+++ server/inngest/index.js
@@ -0,0 +1,92 @@
+import { Inngest } from "inngest";
+import prisma from '../configs/prisma.js'
+
+// create a client to send and reveive events
+export const inngest = new Inngest({id:"profile-marketplace"});
+
+// Inngest function to save user data to a database
+
+const syncUserCreation = inngest.createFunction(
+    {id: "sync-user-from-clerk"},
+    {event: "clerk/user.created"},
+    async({event})=>{
+        const {data} = event
+
+        const user = await prisma.user.findFirst({
+            where:{id: data.id}
+        })
+
+        if(user){
+            await prisma.user.update({
+                where :{id: data.id},
+                data:{
+                    email: data?.email_addresses[0]?.email_address,
+                    name: data?.first_name+" "+ data?.last_name,
+                    image: data?.image_url,
+                }
+            })
+            return;
+        }
+        await prisma.user.create({
+                data:{
+                    id: data.id,
+                    email: data?.email_addresses[0]?.email_address,
+                    name: data?.first_name+" "+ data?.last_name,
+                    image: data?.image_url,
+                }
+            })
+    }
+)
+
+// innjest function to delete user from database
+const syncUserDeletion = inngest.createFunction(
+    {id: "sync-user-from-clerk"},
+    {event: "clerk/user.deleted"},
+    async({event})=>{
+        const {data} = event
+
+        const listings = await prisma.listing.findMany({
+            where: {ownerId : data.id}
+        })
+
+        const chats = await prisma.chat.findMany({
+            where: {OR : [{ownerUserId: data.id}, {chatUserId : data.id}]}
+        })
+
+        const transactions = await prisma.transaction.findMany({
+            where: {userId: data.id}
+        })
+        if(listings.length===0 && chats.length===0 && transactions.length ===0){
+            await prisma.user.delete({where:{id: data.id}})
+        }else{
+            await prisma.listing.updateMany({
+                where:{ownerID: data.id},
+                data:{status:"inactive"}
+            })
+        }
+    }
+)
+// innjest function to update user data in database
+const syncUserUpdation = inngest.createFunction(
+    {id: "update-user-from-clerk"},
+    {event: "clerk/user.created"},
+    async({event})=>{
+        const {data} = event
+
+        await prisma.user.update({
+            where: {id: data.id},
+                data:{
+                    email: data?.email_addresses[0]?.email_address,
+                    name: data?.first_name+" "+ data?.last_name,
+                    image: data?.image_url,
+                }
+            })
+    }
+)
+
+// export an empty array where we'all export future Inngest functions
+export const functions=[
+    syncUserCreation,
+    syncUserDeletion,
+    syncUserUpdation
+];
--- server/server.js
+++ server/server.js
@@ -2,15 +2,19 @@ import express from "express"
 import 'dotenv/config'
 import cors from "cors"
 import { clerkMiddleware } from '@clerk/express'
+import {serve} from 'inngest/express';
+import {inngest, functions} from "./inngest/index.js"
 
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); 
 app.use(cors())
 app.use(clerkMiddleware())
 
 app.get("/", (req, res) => res.send("server is live!"))
+
+app.use("/api/inngest", serve({client:inngest, functions}))
 
 const PORT = process.env.PORT || 3000;
 
